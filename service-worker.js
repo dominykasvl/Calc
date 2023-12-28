a3e388d98e069f795091946977284803abd7e72b
Dominykas Volodka <dominykas.volodka@icloud.com>
2023-12-28 18:32:25 +0200

    Added web page for testing. Service worker for caching cdn's

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+var CACHE_NAME = 'my-cache';
+var urlsToCache = [
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://unpkg.com/jspdf-autotable@3.8.1/dist/jspdf.plugin.autotable.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
